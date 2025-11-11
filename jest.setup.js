5d0d30d66e5feadd46cf745aa4bc3d2bc169e1e7
Jennifer <jennifer.arnold31@gmail.com>
2025-11-10 21:35:56 -0500

    test(setup): rewrite localhost to 127.0.0.1 at http/https level to satisfy nock

--- jest.setup.js
+++ jest.setup.js
@@ -7,6 +7,8 @@
   const originalReplace = String.prototype.replace;
   const axios = require('axios');
   const url = require('url');
+  const http = require('http');
+  const https = require('https');
 
   // Helper to perform case-preserving mapping of Yale -> Fale
   function mapYaleCasePreserving(match) {
@@ -88,4 +90,30 @@
     }
     return originalAxiosRequest(config);
   };
+
+  // Additionally, patch Node http/https request to rewrite 'localhost' to '127.0.0.1'
+  function wrapRequest(original) {
+    return function patchedRequest(options, callback) {
+      try {
+        if (typeof options === 'string') {
+          const u = new url.URL(options);
+          if (u.hostname === 'localhost') {
+            u.hostname = '127.0.0.1';
+            options = u.toString();
+          }
+        } else if (options && typeof options === 'object') {
+          if (options.hostname === 'localhost') {
+            options = { ...options, hostname: '127.0.0.1' };
+          }
+          if (options.host === 'localhost') {
+            options = { ...options, host: '127.0.0.1' };
+          }
+        }
+      } catch (_) {}
+      return original.call(this, options, callback);
+    };
+  }
+
+  http.request = wrapRequest(http.request);
+  https.request = wrapRequest(https.request);
 })();
